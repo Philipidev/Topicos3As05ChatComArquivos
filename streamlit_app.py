b1c6a30b68cd67cef41cfc2b754da45a20db55a0
Philipicalt <philipigariglio@gmail.com>
2025-06-28 12:46:12 -0300

    fix streamlistapp

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
-from run import main
-main()
+# streamlit_app.py
+import sys
+import os
+
+# Garante que os módulos backend sejam encontrados
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'backend')))
+
+# Agora chama a interface diretamente
+from frontend import app  # frontend/app.py será executado
